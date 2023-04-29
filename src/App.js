1971c116bc5b9cb8cc0916447524524814d9d7b3
johnbuenodev <johnbuenodev@gmail.com>
2023-04-29 18:43:21 -0300

    Lembrando conceitos de reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useEffect, useState } from "react";
+
+import './styles.css';
 
 function App() {
+
+  //let url1 = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+  //let url2 = 'https://jsonplaceholder.typicode.com/todos';
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+
+    loadAPI();
+
+  },[]);
+
+  function loadAPI() {
+    let url2 = 'https://jsonplaceholder.typicode.com/todos';
+    fetch(url2)
+    .then((res) => res.json()) //convertendo json
+    .then((jsonValue) => {
+      setTodos(jsonValue);
+      console.log(jsonValue);
+    })
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <div className="title">
+      <h2 className="title-text">Lista de TODOS</h2>
+     </div>
+    
+
+     <br/>
+     <div className="list">
+        <ul className="container-list">
+        { todos.map( todo => {
+            return(
+                 <li key={todo.nome} className="lista-custom" >
+                  <div className="card">
+                    <span className="info">User ID: {todo.userId} </span>
+                    <span className="info">ID: {todo?.id} </span>
+                    <span className="info">TITLE: {todo?.title} </span>
+                    <span className="info">COMPLETED: {todo?.completed} </span>
+                  </div>
+                 </li>
+            )   
+        })}
+        </ul>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
+
+/*
+
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+
+
+*/
